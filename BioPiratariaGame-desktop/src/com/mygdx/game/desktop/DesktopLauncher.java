3b5bf1e98bbe524a909da9a00958e0488a72d531
anny_ <anny_@LAPTOP-MAK50N8D>
2018-05-04 20:27:19 -0300

    alt

--- BioPiratariaGame-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ BioPiratariaGame-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,5 +1,5 @@
 package com.mygdx.game.desktop;
-//alterando
+//...
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.BioPirataria;
