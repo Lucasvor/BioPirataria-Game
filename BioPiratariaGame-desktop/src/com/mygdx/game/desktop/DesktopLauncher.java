8a24ce9871351b04bf4ab88a27ed1dfbfec8e239
Lucas <Lucas@github.com>
2018-05-05 00:16:06 -0300

    teste outra conta

--- BioPiratariaGame-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ BioPiratariaGame-desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,5 +1,5 @@
 package com.mygdx.game.desktop;
-//...
+//... teste outra conta
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.BioPirataria;
